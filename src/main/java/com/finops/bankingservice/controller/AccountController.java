9138c83d7db482af1e90d0c7eacd3475771330a4
Raghavendra Prasad <raghavendra.prasad@dltapps.co.uk>
2023-05-10 10:48:14 +0530

    get operations

--- src/main/java/com/finops/bankingservice/controller/AccountController.java
+++ src/main/java/com/finops/bankingservice/controller/AccountController.java
@@ -1,14 +1,29 @@
 package com.finops.bankingservice.controller;
 
+import org.bson.Document;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AccountController {
 
-    @PostMapping("/create-account")
-    public String createAccount(){
+    @Autowired
+    private MongoTemplate mongoTemplate;
 
-        return "Account Created successfully";
+    @GetMapping("/create-account/{name}/{age}/{gender}")
+    public Document createAccount(@PathVariable String name, @PathVariable int age, @PathVariable String gender){
+
+        Document account = new Document();
+        account.put("accountHolderName", name);
+        account.put("holderAge", age);
+        account.put("Gender", gender);
+
+        Document newBankAccounts = mongoTemplate.save(account, "NewBankAccounts");
+        return newBankAccounts;
+
     }
 }
